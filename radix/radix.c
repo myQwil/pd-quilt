12c995e330f8bdce479f1ff6e2128f14d7ba0bbf
Mike <mike.willmot@gmail.com>
2016-12-11 10:44:12 -0500

    radix update
    negatives are better represented.

--- radix/radix.c
+++ radix/radix.c
@@ -13,29 +13,30 @@ static void radix_float(t_radix *x, t_float f) {
 	t_float b;
 	char digit[32] = "0123456789NECDAFGHJKMPQRSTUVWXYZ";
 	int len=8, new[len],
-		i=0, j, size, dp, flot=0,
-		d=f, rad=x->x_rad;
+		i=0, j=0, dp, size, flot=0,
+		d=f, rad=x->x_rad, neg=(f<0);
 	if (rad<2) rad=2; else if (rad>32) rad=32;
 
-	do{ new[i++] = (d<0?-1:1)*(d%rad); d/=rad; }
-	while (d!=0 && i<len);
+	do{ new[i++%len] = (d<0?-1:1)*(d%rad); d/=rad; }
+	while (d!=0);
 	dp=i; // decimal point
 	
-	if (i<len-1 && (flot=(f!=(d=f))))
+	if (dp<len-1 && (flot=(f!=(d=f))))
 	{	do{ f-=d; f*=(f<0?-1:1)*rad;
 			d=f, b=f-d; d+=(b-.999>=0);
-			if (b>-.005 && b<.005 && d==0) f=0;
+			if (b>-.001 && b<.001 && d==0) f=0;
 			else new[i++] = d; }
 		while (f!=0 && i<len);   }
 	
-	size=i+flot;
+	size=(i>len?len:i)+flot+neg;
 	char buf[size+1];
-	for (j=0, i=dp-1; j<dp; j++, i--)
-		buf[j] = digit[new[i]];
+	if (neg) buf[j++] = '-';
+	for (i=dp-1; j<(dp>len?len:dp)+neg; j++, i--)
+		buf[j] = digit[new[i%len]];
 	if (flot)
 	{	buf[j++] = '.';
 		for (; j<size; j++)
-			buf[j] = digit[new[j-1]];   }
+			buf[j] = digit[new[j-flot-neg]];   }
 	
 	buf[size] = '\0';
 	outlet_symbol(x->x_obj.ob_outlet, gensym(buf));
