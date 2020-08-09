a6ee843d6f167b83be8c4dc1ced83e9db4c3ae8c
myQwil <myQwil@gmail.com>
2020-08-08 20:09:52 -0400

    change gme source files from .c to .cpp
    Linux doesn't need this, but Windows does.

--- gme/gmepd.h
+++ gme/gmepd.h
@@ -16,6 +16,15 @@ Shay Green <gblargg@gmail.com>
 #define NCH 2
 #endif
 
+#if defined MSW                   // when compiling for Windows
+#define EXPORT __declspec(dllexport)
+#elif __GNUC__ >= 4               // else, when compiling with GCC 4.0 or higher
+#define EXPORT __attribute__((visibility("default")))
+#endif
+#ifndef EXPORT
+#define EXPORT                    // empty definition for other cases
+#endif
+
 #undef byte
 #define byte byte_
 typedef unsigned char byte;
