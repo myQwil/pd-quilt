4de22da5d76cc5238381846a924150a54bd0a9dd
Mike Will <myQwil@gmail.com>
2022-02-10 01:03:30 -0500

    slope: added list and anything methods
    for setting all 3 args in one message

--- slope.h
+++ slope.h
@@ -47,13 +47,27 @@ static void slope_log(t_slope *x ,t_float f) {
 	slope_k(x);
 }
 
+static void slope_list(t_slope *x ,t_symbol *s ,int ac ,t_atom *av) {
+	switch (ac)
+	{ case 3: if (av[2].a_type == A_FLOAT) x->run = atom_getfloat(av+2); // no break
+	  case 2: if (av[1].a_type == A_FLOAT) x->max = atom_getfloat(av+1); // no break
+	  case 1: if (av[0].a_type == A_FLOAT) x->min = atom_getfloat(av+0); slope_k(x);  }
+}
+
+static void slope_anything(t_slope *x ,t_symbol *s ,int ac ,t_atom *av) {
+	t_atom atoms[ac+1];
+	atoms[0] = (t_atom){.a_type=A_SYMBOL ,.a_w={.w_symbol = s}};
+	memcpy(atoms+1 ,av ,ac * sizeof(t_atom));
+	slope_list(x ,0 ,ac+1 ,atoms);
+}
+
 static t_slope *slope_new(t_class *cl ,int ac ,t_atom *av) {
 	t_slope *x = (t_slope*)pd_new(cl);
 	outlet_new(&x->obj ,&s_float);
 	inlet_new (&x->obj ,&x->obj.ob_pd ,&s_float ,gensym("min"));
 	inlet_new (&x->obj ,&x->obj.ob_pd ,&s_float ,gensym("max"));
 	inlet_new (&x->obj ,&x->obj.ob_pd ,&s_float ,gensym("run"));
-	t_float min=0 ,max=1 ,run=100;
+	t_float min=0 ,max=1 ,run=128;
 
 	if (ac && av->a_type == A_SYMBOL)
 	{	x->log = !strcmp(av->a_w.w_symbol->s_name ,"log");
@@ -76,6 +90,9 @@ static t_slope *slope_new(t_class *cl ,int ac ,t_atom *av) {
 
 static t_class *slope_setup(t_symbol *s ,t_newmethod newm) {
 	t_class *xclass = class_new(s ,newm ,0 ,sizeof(t_slope) ,0 ,A_GIMME ,0);
+	class_addlist    (xclass ,slope_list);
+	class_addanything(xclass ,slope_anything);
+
 	class_addmethod(xclass ,(t_method)slope_min ,gensym("min") ,A_FLOAT ,0);
 	class_addmethod(xclass ,(t_method)slope_max ,gensym("max") ,A_FLOAT ,0);
 	class_addmethod(xclass ,(t_method)slope_run ,gensym("run") ,A_FLOAT ,0);
