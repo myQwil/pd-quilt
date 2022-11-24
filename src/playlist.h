08206f9e27683a22f6211ec197294a53ea99a8c5
Mike Will <myQwil@gmail.com>
2022-11-24 14:30:57 -0500

    ffplay~: prevent self-referencing m3u's from causing an infinite loop

--- src/playlist.h
+++ src/playlist.h
@@ -9,55 +9,49 @@ typedef struct {
 	int max;        /* size of the memory allocation */
 } t_playlist;
 
-static int m3u_size(FILE *fp, char *dir, int dlen) {
-	int size = 0;
-	char line[MAXPDSTRING];
-	while (fgets(line, MAXPDSTRING, fp) != NULL) {
-		line[strcspn(line, "\r\n")] = '\0';
-		int isabs = (line[0] == '/');
-		if ((isabs ? 0 : dlen) + strlen(line) >= MAXPDSTRING) {
-			continue;
-		}
-		char *ext = strrchr(line, '.');
-		if (ext && !strcmp(ext + 1, "m3u")) {
-			strcpy(dir + dlen, line);
-			char *fname = strrchr(line, '/');
-			int len = (fname) ? ++fname - line : 0;
-			FILE *m3u = fopen(dir, "r");
-			if (m3u) {
-				size += m3u_size(m3u, dir, dlen + len);
-				fclose(m3u);
-			}
-		} else {
-			size++;
-		}
+	// prevents self-referencing m3u's from causing an infinite loop
+static int depth;
+
+#define M3U_MAIN(deeper, increment) \
+	char line[MAXPDSTRING]; \
+	while (fgets(line, MAXPDSTRING, fp) != NULL) { \
+		line[strcspn(line, "\r\n")] = '\0'; \
+		int isabs = (line[0] == '/'); \
+		if ((isabs ? 0 : dlen) + strlen(line) >= MAXPDSTRING) { \
+			continue; \
+		} \
+		strcpy(dir + dlen, line); \
+		char *ext = strrchr(line, '.'); \
+		if (ext && !strcmp(ext + 1, "m3u")) { \
+			char *fname = strrchr(line, '/'); \
+			int len = (fname) ? ++fname - line : 0; \
+			FILE *m3u = fopen(dir, "r"); \
+			if (m3u && depth < 0x100) { \
+				depth++; \
+				(deeper); \
+				depth--; \
+				fclose(m3u); \
+			} \
+		} else { \
+			(increment); \
+		} \
 	}
+
+static inline int m3u_size(FILE *fp, char *dir, int dlen) {
+	int size = 0;
+	M3U_MAIN (
+	  size += m3u_size(m3u, dir, dlen + len)
+	, size++
+	)
 	return size;
 }
 
-static int playlist_fill(t_playlist *pl, FILE *fp, char *dir, int dlen, int i) {
-	char line[MAXPDSTRING];
+static inline int playlist_fill(t_playlist *pl, FILE *fp, char *dir, int dlen, int i) {
 	int oldlen = strlen(pl->dir->s_name);
-	while (fgets(line, MAXPDSTRING, fp) != NULL) {
-		line[strcspn(line, "\r\n")] = '\0';
-		int isabs = (line[0] == '/');
-		if ((isabs ? 0 : dlen) + strlen(line) >= MAXPDSTRING) {
-			continue;
-		}
-		strcpy(dir + dlen, line);
-		char *ext = strrchr(line, '.');
-		if (ext && !strcmp(ext + 1, "m3u")) {
-			char *fname = strrchr(line, '/');
-			int len = (fname) ? ++fname - line : 0;
-			FILE *m3u = fopen(dir, "r");
-			if (m3u) {
-				i = playlist_fill(pl, m3u, dir, dlen + len, i);
-				fclose(m3u);
-			}
-		} else {
-			pl->arr[i++] = gensym(dir + oldlen);
-		}
-	}
+	M3U_MAIN (
+	  i = playlist_fill(pl, m3u, dir, dlen + len, i)
+	, pl->arr[i++] = gensym(dir + oldlen)
+	)
 	return i;
 }
 
@@ -67,9 +61,13 @@ static inline err_t playlist_m3u(t_playlist *pl, t_symbol *s) {
 		return "Could not open m3u";
 	}
 
+	depth = 1;
 	char dir[MAXPDSTRING];
 	strcpy(dir, pl->dir->s_name);
 	int size = m3u_size(fp, dir, strlen(dir));
+	if (size < 1) {
+		return "Playlist is empty";
+	}
 	if (size > pl->max) {
 		pl->arr = (t_symbol **)resizebytes(pl->arr
 		, pl->max * sizeof(t_symbol *), size * sizeof(t_symbol *));
